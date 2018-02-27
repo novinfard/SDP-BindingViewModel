0c4460db3dfe359dc471daedab688f3eeff14fb4
Soheil Novinfard <novinfard@gmail.com>
2018-02-27 17:07:01 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Binding ViewModel in iOS - MVVM Design Pattern
+This IOS app demonstrates unit tests and user interface tests using Xcode's built-in testing framework.
+
+## Main Targets
+MVVM
+
+## Credits
+The concept of this app is adapted from Swift Design Patterns written by Paul Hudson.
+
+## Author
+**Soheil Novinfard** - [www.novinfard.com](https://www.novinfard.com)
+
+## License
+This project is licensed under the MIT License.
---
+++ SDP-BindingViewModelTests/SDP_BindingViewModelTests.swift
@@ -0,0 +1,36 @@
+//
+//  SDP_BindingViewModelTests.swift
+//  SDP-BindingViewModelTests
+//
+//  Created by Soheil on 27/02/2018.
+//  Copyright © 2018 Soheil Novinfard. All rights reserved.
+//
+
+import XCTest
+@testable import SDP_BindingViewModel
+
+class SDP_BindingViewModelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
